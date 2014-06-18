678def4bdec2ab0801da4973f0492d1a2a0ad11b
tuanbui98 <kuti.vip_kute@yahoo.com>
2014-06-18 09:29:13 +0700

    version to upload

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "Chúc mừng bạn đã thắng cuộc!" : "Đồ cùi bắp!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
